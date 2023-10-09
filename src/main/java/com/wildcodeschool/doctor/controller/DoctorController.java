dd5f8ac6958b4936b95b7276a9003f36d28b1fa7
Elodie <elodie.dargot@gmail.com>
2023-10-09 20:55:54 +0200

    exercice terminé-coreection routes 404

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -16,9 +16,11 @@ public class DoctorController {
     public Doctor getDoctor(@PathVariable int number) {
         if(number == 13){
             return new Doctor( 13, "Jodie Whittaker");
-        } else if (number == 42){
+        } else if (number >= 1 && number <= 12){
+            throw new ResponseStatusException(HttpStatus.SEE_OTHER);
+        } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation " + number);
         }
-        throw new ResponseStatusException(HttpStatus.SEE_OTHER, "Je suis une erreur 303! ") ;
     }
 }
+
